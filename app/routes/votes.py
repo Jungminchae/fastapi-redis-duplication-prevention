f44a0b4e3c73662a35e3335d098bf714e67a42d0
Jungminchae <minchae3618@gmail.com>
2024-04-01 18:04:56 +0900

    add api

--- app/routes/votes.py
+++ app/routes/votes.py
@@ -1,9 +1,11 @@
 from fastapi import APIRouter
 
+# from app.orms.votes import vote_orm
+from app.schemas.votes import VoteRequest
 
 router = APIRouter()
 
 
 @router.post("/")
-async def create_vote():
+async def create_vote(data: VoteRequest):
     ...
